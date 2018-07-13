8ba34d28c13505732695f5f0fb13fe80a990b714
Benjamin Devaublanc <benjamin.devaublanc@gmail.com>
2018-07-13 14:08:20 +0200

    install mongoose

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
 
-app.get('/', function(req, res){
-  res.send("Hello World");
+//DB setup
+// mongoose.connect("mongodb://mongo:27017/test");
+mongoose.connect("mongodb://localhost:27017/test");
+
+
+const Fruit = mongoose.model('Fruit', { name: String });
+
+app.get('/', function(req, res){    
+    res.send("Hello World-changed-2");
 });
+
+app.get('/list', function(req, res){    
+    Fruit.find({},{}, function(e, docs){
+        console.log(docs)
+        res.send(docs);
+    });
+    
+});
+
+app.get('/add', function(req, res){
+    const pomme = new Fruit({ name: 'Pomme' });
+    pomme.save().then(() => res.send('Pomme saved'));
+});
+
 
 app.listen(3000, function(){
   console.log('Example app listening on port 3000!');
